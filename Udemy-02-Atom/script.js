6dab9b34319ebdc891ccb660287fbbcbde31f8f9
Emilie Décosterd <emilie.decosterd@gmail.com>
2022-10-08 10:45:55 +0200

    Effect mousemove

--- Udemy-02-Atom/script.js
+++ Udemy-02-Atom/script.js
@@ -5,17 +5,27 @@ canvas.height = window.innerHeight;
 
 let atoms = [];
 
-canvas.addEventListener('click', function(e){
-  for (let i = 0; i < 5; i++) {
+canvas.addEventListener('mousemove', function(e){
+  for (let i = 0; i < 15; i++) {
     atoms.push(new Atom(e.x, e.y));
   }
 });
 
 const animate = () => {
-  atoms.forEach(atom => {
+  atoms.forEach((atom, index) => {
     atom.draw();
-    atom.update();
-  })
+    atom.updateSpeed();
+    atom.updateSize();
+
+    if(atom.radius < 0.3){
+      atoms.splice(index, 1);
+    }
+  });
+  ctx.save();
+  ctx.fillStyle = 'rgba(255, 255, 255, 0.2)';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  ctx.restore();
   requestAnimationFrame(animate)
 }
 
@@ -30,9 +40,13 @@ class Atom {
     this.speedY = Math.random() * 4 - 2;
   }
 
-  update(){
+  updateSpeed(){
     this.x += this.speedX;
     this.y += this.speedY;
+  }
+
+  updateSize(){
+    this.radius -= 0.1;
   }
 
   draw(){
