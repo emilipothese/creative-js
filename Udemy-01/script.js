40c6eb28d264da0bab854684b631226705f0d2b2
Emilie Décosterd <emilie.decosterd@gmail.com>
2022-10-08 10:24:46 +0200

    fill and stroke rectangle and round OK

--- Udemy-01/script.js
+++ Udemy-01/script.js
@@ -1,2 +1,30 @@
 const canvas = document.getElementById('my-canvas');
 const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+/**
+ * Click to add point
+ */
+// canvas.addEventListener('click', function (e){
+//   ctx.beginPath();
+//   ctx.rect(e.x, e.y, 10, 10); // 2 premiers position, 2 suivants taille de l'élément
+//   ctx.stroke();
+// })
+
+/**
+ * mouse move and draw
+ */
+// canvas.addEventListener('mousemove', function (e){
+//   ctx.beginPath();
+//   ctx.rect(e.x, e.y, 10, 10);
+//   ctx.stroke();
+// })
+
+const degToRad = (deg) => {
+  return deg / 180 * Math.PI;
+}
+
+ctx.beginPath();
+ctx.arc(100, 100, 50, 0, degToRad(180));
+ctx.stroke();
