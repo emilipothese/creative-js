3dccd148c58d16601f45779c959b36b6d5180f3a
Emilie Décosterd <emilie.decosterd@gmail.com>
2022-11-22 15:02:54 +0100

    Add JS folder

--- Floema/app/index.js
+++ Floema/app/index.js
@@ -1,1 +1,21 @@
-console.log('hello world!')
+import About from 'pages/About'
+import Collections from 'pages/Collections'
+import Detail from 'pages/Detail'
+import Home from 'pages/Home'
+
+class App {
+  constructor () {
+
+  }
+
+  createPages () {
+    this.pages = {
+      'home': new Home(),
+      'collections': new Collections(),
+      'detail': new Detail(),
+      'about': new About()
+    }
+  }
+}
+
+new App()
